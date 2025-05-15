4a842a0f122751ed085089692c8f9873e3fc3c33
Caio Lemos <caio_lemos94@hotmail.com>
2025-05-15 15:50:03 -0300

    feat: add task routes to api.php with CRUD operations

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+/* Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:sanctum'); */
+
+
+Route::prefix('tasks')->name('tasks.')->group(function () {
+    Route::get('/', [TaskController::class, 'index'])->name('index');
+    Route::get('/{task}', [TaskController::class, 'show'])->name('show');
+    Route::post('/', [TaskController::class, 'store'])->name('store');
+    Route::put('/{task}', [TaskController::class, 'update'])->name('update');
+    Route::delete('/{task}', [TaskController::class, 'destroy'])->name('destroy');
+});

add76c4e2cd79f7271fd80b9161ad6574566cc15
Caio Lemos <caio_lemos94@hotmail.com>
2025-05-15 16:47:54 -0300

    feat: implement form requests for task creation and update validation

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Enums\TaskStatus;
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
 use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
@@ -22,26 +24,17 @@ class TaskController extends Controller
     return response()->json($task);
   }
 
-  public function store(Request $request)
+  public function store(StoreTaskRequest $request)
   {
-    $data = $request->validate([
-      'title' => 'required|string',
-      'description' => 'string|nullable',
-      'status' => [Rule::enum(TaskStatus::class)]
-    ]);
+    $data = $request->validated();
 
     $task = Task::create($data);
     return response()->json($task, 201, ['Location' => route('tasks.show', $task->id)]);
   }
 
-  public function update(Request $request, Task $task)
+  public function update(UpdateTaskRequest $request, Task $task)
   {
-    $data = $request->validate([
-      'title' => 'string',
-      'description' => 'string|nullable',
-      'status' => [Rule::enum(TaskStatus::class)]
-    ]);
-
+    $data = $request->validated();
 
     $task->update($data);
     return response()->json($task);
